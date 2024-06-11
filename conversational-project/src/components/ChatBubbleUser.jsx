9db21c0d3be075a6f2aa9541e97d9eaea538889c
NicoDipi <nicolasfelipedipierro@gmail.com>
2024-06-11 14:25:43 -0300

    *Styles

--- conversational-project/src/components/ChatBubbleUser.jsx
+++ conversational-project/src/components/ChatBubbleUser.jsx
@@ -7,10 +7,10 @@ const ChatBubbleUser = () =>
             <div className="chat-dot-0 user-dot"></div>
             <div className="chat-dot-1 user-dot"></div>
             <div className="center chat-content user-bg">
-                <p className="chat-text user-color">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum quia ducimus amet aperiam quasi nulla veniam ipsa temporibus officiis eum mollitia qui adipisci blanditiis placeat, pariatur, commodi, ab quae. Fugit.</p>
                 <button className="center chat-audio">
                     <Icon className="chat-audio-icon user-color" icon="ant-design:sound-filled" />
                 </button>
+                <p className="chat-text user-color">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum quia ducimus amet aperiam quasi nulla veniam ipsa temporibus officiis eum mollitia qui adipisci blanditiis placeat, pariatur, commodi, ab quae. Fugit.</p>
             </div>
         </div>
     )
---
+++ conversational-project/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import Input from "./Input.jsx"
+import Listener from "./Listener"
+
+const Footer = () =>
+{
+    return (
+        <div className="center footer">
+            <Input/>
+            <Listener/>
+        </div>
+    )   
+}
+
+export default Footer;
